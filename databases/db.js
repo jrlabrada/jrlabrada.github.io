97a4f085bafffa66a63b80b853142330fde62ba8
jrlabrada <jrlabrada2019@gmail.com>
2022-03-07 17:19:24 +0100

    1.28.3

--- databases/db.js
+++ databases/db.js
@@ -4,7 +4,7 @@ const conexion = mysql.createConnection({
     host : 'us-cdbr-east-05.cleardb.net',
     user : 'b49f461dc92e98',
     password : 'a73a5e2f',
-    database : 'heroku_93dd851c0deb5df',
+    database : 'heroku_93dd851c0deb5df'
 })
 
 conexion.connect((error)=>{
